a6dbf184139eb2cfd3ee8fdce22d89679046f6a8
wshin025 <wody0506@gmail.com>
2024-08-29 16:54:39 +0900

    react 2,3

---
+++ src/chp02/myBytton.js
@@ -0,0 +1,16 @@
+// import ReactDOM from "react-dom/client";
+// import React from "react";
+
+function  myBytton(props) {
+    const [isClicked,setIsClicked] = React.useState(false)
+    return React.createElement(
+        'button',
+        {
+            onClick : ()  => setIsClicked(true)},
+    isClicked ? 'Clicked!' : 'Click here~~'
+)
+}
+
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement(myBytton));
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './chp03/Book.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Library from "./chp03/Library";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Library />
+    </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
